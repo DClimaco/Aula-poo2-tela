4538c1e02c3bdc69a68ab1819b4a2d19a4fa9a7a
Daniel Carvalho Climaco <111577285+DClimaco@users.noreply.github.com>
2024-05-02 08:36:27 -0300

    Add files via upload

---
+++ Aula-20240502-tela/Homer.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Aula_20240502_tela
+{
+    partial class Homer
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.lblContador = new System.Windows.Forms.Label();
+            this.btnMenos = new System.Windows.Forms.Button();
+            this.btnVerde = new System.Windows.Forms.Button();
+            this.btnAzul = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(137, 151);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(92, 44);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Click";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // lblContador
+            // 
+            this.lblContador.AutoSize = true;
+            this.lblContador.Location = new System.Drawing.Point(248, 117);
+            this.lblContador.Name = "lblContador";
+            this.lblContador.Size = new System.Drawing.Size(13, 13);
+            this.lblContador.TabIndex = 1;
+            this.lblContador.Text = "0";
+            this.lblContador.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnMenos
+            // 
+            this.btnMenos.Location = new System.Drawing.Point(271, 151);
+            this.btnMenos.Name = "btnMenos";
+            this.btnMenos.Size = new System.Drawing.Size(92, 44);
+            this.btnMenos.TabIndex = 2;
+            this.btnMenos.Text = "Menos";
+            this.btnMenos.UseVisualStyleBackColor = true;
+            this.btnMenos.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnVerde
+            // 
+            this.btnVerde.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnVerde.ForeColor = System.Drawing.Color.Black;
+            this.btnVerde.Location = new System.Drawing.Point(451, 133);
+            this.btnVerde.Name = "btnVerde";
+            this.btnVerde.Size = new System.Drawing.Size(75, 23);
+            this.btnVerde.TabIndex = 3;
+            this.btnVerde.Text = "Verde";
+            this.btnVerde.UseVisualStyleBackColor = false;
+            this.btnVerde.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btnAzul
+            // 
+            this.btnAzul.BackColor = System.Drawing.Color.Blue;
+            this.btnAzul.Location = new System.Drawing.Point(451, 172);
+            this.btnAzul.Name = "btnAzul";
+            this.btnAzul.Size = new System.Drawing.Size(75, 23);
+            this.btnAzul.TabIndex = 4;
+            this.btnAzul.Text = "Azul";
+            this.btnAzul.UseVisualStyleBackColor = false;
+            this.btnAzul.Click += new System.EventHandler(this.btnAzul_Click);
+            // 
+            // Homer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 363);
+            this.Controls.Add(this.btnAzul);
+            this.Controls.Add(this.btnVerde);
+            this.Controls.Add(this.btnMenos);
+            this.Controls.Add(this.lblContador);
+            this.Controls.Add(this.btnClick);
+            this.Name = "Homer";
+            this.Text = "Homer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Label lblContador;
+        private System.Windows.Forms.Button btnMenos;
+        private System.Windows.Forms.Button btnVerde;
+        private System.Windows.Forms.Button btnAzul;
+    }
+}
+
---
+++ Aula-20240502-tela/Homer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula_20240502_tela
+{
+    public partial class Homer : Form
+    {
+        public Homer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            int cont = 0;
+            cont = int.Parse(lblContador.Text);
+            cont++;
+
+            lblContador.Text = cont.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int cont = 0;
+            cont = int.Parse(lblContador.Text);
+            cont--;
+
+            lblContador.Text = cont.ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            BackColor = Color.Green;
+        }
+
+        private void btnAzul_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.Blue;
+        }
+    }
+}
